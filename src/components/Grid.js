12272f83b2c835817d622b868fd8e0bc953aa2d6
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2020-07-24 15:53:43 -0500

    Moved event listener to componentDidMount to avoid deprecation warning

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default class Grid extends React.Component {
-    componentWillMount() {
+    componentDidMount() {
         //Add keyboard controls
         window.addEventListener('keydown', this.handleKeyPress.bind(this));
     }
